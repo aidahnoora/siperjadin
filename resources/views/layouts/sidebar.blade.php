8f2afbf106f5bd6dcb0036e1eb9598fb2922457f
A'idah Noor <webinaraidah30@gmail.com>
2022-06-14 09:14:07 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,40 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="index.html">SI PERJADIN</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">SP</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Dashboard</li>
+        <li class="active">
+            <a href="#" class="nav-link">
+                <i class="fas fa-fire"></i><span>Dashboard</span>
+            </a>
+        </li>
+        <li class="menu-header">Menu Utama</li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
+                <i class="fas fa-columns"></i> <span>Data Pendukung</span>
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="#">Pegawai</a></li>
+                <li><a class="nav-link" href="#">Tujuan Perjalanan</a></li>
+                <li><a class="nav-link" href="#">Bendahara</a></li>
+                <li><a class="nav-link" href="#">Instansi</a></li>
+                <li><a class="nav-link" href="#">Anggaran</a></li>
+                <li><a class="nav-link" href="#">Dasar SPT</a></li>
+            </ul>
+        </li>
+        <li>
+            <a class="nav-link" href="#">
+                <i class="far fa-square"></i> <span>Pengisian SPPD</span>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link" href="#">
+                <i class="far fa-square"></i> <span>Laporan</span>
+            </a>
+        </li>
+    </ul>
+</aside>
